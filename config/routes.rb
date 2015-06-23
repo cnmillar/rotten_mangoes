b036fcef8bfcf5f7f383c24167c7d45b3f8082e7
cnmillar <cnmillar@gmail.com>
2015-06-23 19:08:03 +0000

    add reviews to movies#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 RottenMangues::Application.routes.draw do
 
+  get "reviews/new"
+  get "reviews/create"
   get "sessions/new"
   get "sessions/create"
   get "users/new"
   get "users/create"
-  resources :movies
+  
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
