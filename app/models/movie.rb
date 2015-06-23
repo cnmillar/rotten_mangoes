8bc0e9407dec3bc46c523d4f9f0a022f6dc98e88
cnmillar <cnmillar@gmail.com>
2015-06-23 19:34:21 +0000

    refactored

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,6 +22,12 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_future	
 
+  def review_average
+    if reviews.size > 0 
+      reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
+    end
+  end
+
   protected
 
   def release_date_is_in_the_future
