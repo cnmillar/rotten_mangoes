8449d5012965048803fc99725ae41ad7931f946a
cnmillar <cnmillar@gmail.com>
2015-06-24 18:03:40 +0000

    create feature complete

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,14 +9,14 @@ class Admin::UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-
+			redirect_to admin_users_path, notice: "Successfully added #{@user.firstname}!"
 		else
-
+			render :new
 		end
 	end
 
 	def index
-		@users = User.all.page(params[:page]).per(1)
+		@users = User.all.page(params[:page]).per(4)
 	end
 
 	def show
