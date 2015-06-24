761057a8853035cd45d49f4754dcd95423304a2a
cnmillar <cnmillar@gmail.com>
2015-06-24 00:58:02 +0000

    add edit and delete features

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,11 @@ class Admin::UsersController < ApplicationController
 
 	before_filter :authorize
 
+	def new
+		@user = User.new
+	end
+
+
 	def index
 		@users = User.all.page(params[:page]).per(1)
 	end
@@ -11,15 +16,22 @@ class Admin::UsersController < ApplicationController
 	end
 
 	def destroy
-
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to admin_users_path
 	end
 
 	def edit
-
+		@user = User.find(params[:id])
 	end
 
 	def update
-
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+    	redirect_to admin_users_path
+    else
+      render :edit
+    end
 	end
 
 	protected
